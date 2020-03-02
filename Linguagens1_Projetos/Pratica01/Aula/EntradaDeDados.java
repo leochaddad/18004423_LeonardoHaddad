5d96681d8f715c7c0c670faf1c859a49021d9d89
Leonardo <leo@MacBook-Pro-de-Leonardo.local>
2020-03-02 12:59:53 -0300

    aula 2

---
+++ Linguagens1_Projetos/Pratica01/Aula/EntradaDeDados.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class EntradaDeDados{
+    public static void main(final String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        String nome;
+        System.out.println("Informe seu nome:");
+        nome=scanner.nextLine();
+        System.out.println("Olá "+nome);
+        double n1, n2;
+        System.out.println("Informe 2 numeros: ");
+        n1=scanner.nextDouble();
+        n2=scanner.nextDouble();
+        System.out.println("A soma é "+ (n1+n2 ) );
+        if(n1>n2){
+            System.out.println("N1 Maior");
+        }
+        else{
+            System.out.println("N2 é maior");
+        }
+   }
+}
---
+++ Linguagens1_Projetos/Pratica01/Aula/MeuPrograma.java
@@ -0,0 +1,6 @@
+class MeuPrograma{
+    public static void main(String[] args){
+        //Mensagem de saida do sistema
+        System.out.println("Olá Mundo\nEstou programando no modo Conan");
+    }
+}
