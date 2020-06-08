b0dac55187ba49e783d4b64d7bcd6bdbfb8ea422
Leonardo Haddad <leonardochaddad@hotmail.com>
2020-06-08 10:14:36 -0300

    Implementação inicial membro

---
+++ Linguagens1_Projetos/Trabalho2/src/br/maua/models/membro.java
@@ -0,0 +1,15 @@
+package br.maua.models;
+
+import br.maua.interfaces.postarMensagem;
+
+public abstract class membro implements postarMensagem {
+    private String nome;
+    private String email;
+
+
+    public void perguntarHorario(){
+
+    }
+
+
+}
