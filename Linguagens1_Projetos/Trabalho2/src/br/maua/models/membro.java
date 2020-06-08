e7958a52bdfb5d0ec10827f8d688dd80f3e14988
Leonardo Haddad <leonardochaddad@hotmail.com>
2020-06-08 10:27:40 -0300

    implementacao de metodos na classe membro e enum horario

--- Linguagens1_Projetos/Trabalho2/src/br/maua/models/membro.java
+++ Linguagens1_Projetos/Trabalho2/src/br/maua/models/membro.java
@@ -1,14 +1,21 @@
 package br.maua.models;
 
+import br.maua.enums.horarios;
 import br.maua.interfaces.postarMensagem;
+
 
 public abstract class membro implements postarMensagem {
     private String nome;
     private String email;
+    private horarios horario;
 
+    public membro(String nome, String email){
+        this.nome = nome;
+        this.email= email;
+    }
 
-    public void perguntarHorario(){
-
+    public horarios perguntarHorario(){
+        return horario;
     }
 
 
