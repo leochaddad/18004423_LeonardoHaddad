56a2a955ae1a5dd575c71e0b86e20d6b7488b8a7
Leonardo Haddad <leonardochaddad@hotmail.com>
2020-06-16 14:59:16 -0300

    Implementacao Usuario

--- Atividades/P2/src/br/maua/models/Usuario.java
+++ Atividades/P2/src/br/maua/models/Usuario.java
@@ -1,4 +1,23 @@
 package br.maua.models;
 
-public class Usuario {
+import br.maua.interfaces.autenticavel;
+
+public class Usuario implements autenticavel {
+
+    private String nome;
+    private String email;
+    private String senha;
+
+
+    public Usuario(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean autenticar(String senha) {
+        return (this.senha.equals(senha));
+    }
+
 }
