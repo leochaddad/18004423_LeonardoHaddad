207299643343a318f37e7c55c126004948caf009
Leonardo Haddad <leonardochaddad@hotmail.com>
2020-06-16 15:39:50 -0300

    Primeira implementacao do sistema

--- Atividades/P2/src/br/maua/Sistema.java
+++ Atividades/P2/src/br/maua/Sistema.java
@@ -1,4 +1,24 @@
 package br.maua;
 
+import br.maua.interfaces.autenticavel;
+import br.maua.models.Pedido;
+
+import java.util.ArrayList;
+
 public class Sistema {
+
+    autenticavel usuario;
+    ArrayList<Pedido> listaPedidos = new ArrayList<>();
+
+    public Sistema(autenticavel usuario) {
+        this.usuario = usuario;
+    }
+
+    void listarPedidos(){
+        for(Pedido pedido:listaPedidos){
+            System.out.println(listaPedidos.indexOf(pedido));
+        }
+    }
+
+
 }
