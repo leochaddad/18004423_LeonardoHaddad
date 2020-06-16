1d3290be2831a44b8d7853cd60e0ff317dd2e56a
Leonardo Haddad <leonardochaddad@hotmail.com>
2020-06-16 15:23:48 -0300

    Implementacao Classe Pedido

--- Atividades/P2/src/br/maua/models/Pedido.java
+++ Atividades/P2/src/br/maua/models/Pedido.java
@@ -1,4 +1,49 @@
 package br.maua.models;
 
+import br.maua.enums.estadosPedido;
+import br.maua.enums.formasDePagamento;
+
+
+import java.util.Random;
+
 public class Pedido {
+    String id;
+    String descricao;
+    float valor;
+    formasDePagamento pagamento;
+    estadosPedido estado;
+    Usuario usuario;
+
+
+    public Pedido(Usuario usuario, String descricao, float valor, formasDePagamento pagamento) {
+        this.descricao = descricao;
+        this.valor = valor;
+        this.pagamento = pagamento;
+        this.estado = estadosPedido.REALIZADO;
+        this.id = this.gerarId();
+        this.usuario = usuario;
+    }
+
+
+    private String gerarId(){
+        Random random = new Random();
+        String idGerado = "";
+        for(int i = 0; i<3; i++){
+            idGerado+= random.nextInt(10);
+        }
+        return idGerado;
+    }
+
+    public void alterarEstado(estadosPedido novoEstado){
+        this.estado = novoEstado;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido: " + id + '\'' +
+                "Descricao: " + descricao + '\'' +
+                "Total: " + valor +
+                "Forma de Pagamento: " + pagamento +
+                "Criado por: " + usuario.getNome() + '\'';
+    }
 }
